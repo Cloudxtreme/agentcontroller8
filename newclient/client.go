8ca63410817a5df655e42f57d304fe5e7eb33aa8
Amr Hassan <amr.hassan@gmail.com>
2015-11-26 16:31:37 +0200

    newclient: DoneResponses() for filtering out non-final responses

--- newclient/client.go
+++ newclient/client.go
@@ -158,3 +158,27 @@ func (c Client) Execute(command *core.Command) <- chan core.CommandResponse {
 
 	return responseChan
 }
+
+// Filters responses and only passes through the success/error final responses
+func DoneResponses(incoming <-chan core.CommandResponse) <-chan core.CommandResponse {
+
+	outgoing := make(chan core.CommandResponse)
+
+	go func() {
+		for {
+			select {
+			case response, isOpen := <-incoming:
+				if !isOpen {
+					close(outgoing)
+					return
+				}
+				state := response.Content.State
+				if state == core.CommandStateSuccess || state == core.CommandStateError {
+					outgoing <- response
+				}
+			}
+		}
+	}()
+
+	return outgoing
+}
