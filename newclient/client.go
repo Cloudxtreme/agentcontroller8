2b01349a45b440e6349d0d9aca15e2600016da2f
Amr Hassan <amr.hassan@gmail.com>
2015-11-26 14:34:46 +0200

    newclient: Removed the timeout parameter from Client.Execute() since it was meaningless

--- newclient/client.go
+++ newclient/client.go
@@ -1,13 +1,17 @@
 package newclient
 import (
-"github.com/garyburd/redigo/redis"
-"github.com/Jumpscale/agentcontroller2/core"
+	"github.com/garyburd/redigo/redis"
+	"github.com/Jumpscale/agentcontroller2/core"
 	"github.com/Jumpscale/agentcontroller2/redisdata/ds"
 	"fmt"
 	"time"
 	"strings"
 	"github.com/Jumpscale/agentcontroller2/utils"
 )
+
+// The timeout used in each blocking response-receiving step. It is not the total timeout for receiving
+// a response.
+const responseTimeout = 1 * time.Minute
 
 type Client struct {
 	connPool *redis.Pool
@@ -48,9 +52,9 @@ func (c Client) Send(command *core.Command) error {
 }
 
 // Blocks and then returns true when the command is picked up, returns false otherwise
-func (c Client) isPickedUp(command *core.Command, timeout time.Duration) bool {
+func (c Client) isPickedUp(command *core.Command) bool {
 	signal := ds.GetList(fmt.Sprintf("cmd.%s.queued", command.Content.ID))
-	data, err := signal.BlockingRightPopLeftPush(c.connPool, timeout, signal)
+	data, err := signal.BlockingRightPopLeftPush(c.connPool, responseTimeout, signal)
 	if err != nil {
 		panic(fmt.Errorf("Redis error: %v", err))
 	}
@@ -89,11 +93,11 @@ func (c Client) responsesFor(command *core.Command) map[core.AgentID]core.Comman
 }
 
 // Blocks and returns true when the specified command on the specified Agent is done, false otherwise
-func (c Client) isDone(command *core.Command, agentID core.AgentID, timeout time.Duration) bool {
+func (c Client) isDone(command *core.Command, agentID core.AgentID) bool {
 	name := fmt.Sprintf("cmd.%s.%d.%d", command.Content.ID, agentID.GID, agentID.NID)
 	signal := ds.GetList(name)
 
-	data, err := signal.BlockingRightPopLeftPush(c.connPool, timeout, signal)
+	data, err := signal.BlockingRightPopLeftPush(c.connPool, responseTimeout, signal)
 	if err != nil {
 		panic(fmt.Errorf("Redis error: %v", err))
 	}
@@ -108,8 +112,8 @@ func (c Client) isDone(command *core.Command, agentID core.AgentID, timeout time
 
 
 
-// Sends a command and returns a channel for reading the response
-func (c Client) Execute(command *core.Command, timeout time.Duration) (<- chan core.CommandResponse, error) {
+// Sends a command and returns a channel for reading the responses
+func (c Client) Execute(command *core.Command) (<- chan core.CommandResponse, error) {
 
 	err := c.Send(command)
 	if err != nil {
@@ -120,7 +124,7 @@ func (c Client) Execute(command *core.Command, timeout time.Duration) (<- chan c
 
 	go func() {
 
-		if !c.isPickedUp(command, timeout) {
+		if !c.isPickedUp(command) {
 			// Something is not right!
 			close(responseChan)
 			return
@@ -146,7 +150,7 @@ func (c Client) Execute(command *core.Command, timeout time.Duration) (<- chan c
 		}
 
 		for agentID, _ := range c.responsesFor(command) {
-			if c.isDone(command, agentID, timeout) {
+			if c.isDone(command, agentID) {
 				responseChan <- c.responsesFor(command)[agentID]
 			}
 		}
