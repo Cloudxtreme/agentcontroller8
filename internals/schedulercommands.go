f1c2646dc7ac4335fd33b737010462cde902faac
Amr Hassan <amr.hassan@gmail.com>
2015-11-30 15:50:34 +0200

    internals: Dropped unused code

--- internals/schedulercommands.go
+++ internals/schedulercommands.go
@@ -1,12 +1,7 @@
 package internals
 import (
-"github.com/Jumpscale/agentcontroller2/scheduling"
-"github.com/Jumpscale/agentcontroller2/core"
-"encoding/json"
-)
-
-const (
-
+	"github.com/Jumpscale/agentcontroller2/scheduling"
+	"github.com/Jumpscale/agentcontroller2/core"
 )
 
 func (manager *Manager) setUpSchedulerCommands(scheduler *scheduling.Scheduler) {
@@ -22,16 +17,7 @@ func (manager *Manager) setUpSchedulerCommands(scheduler *scheduling.Scheduler) 
 
 	manager.commandHandlers[SchedulerListJobs] =
 		func(_ *core.Command) (interface{}, error) {
-			jobs := scheduler.ListJobs()
-			jobsMap := make(map[string]string)
-			for _, job := range jobs {
-				jsonJob, err := json.Marshal(job)
-				if err != nil {
-					panic(err)
-				}
-				jobsMap[job.ID] = string(jsonJob)
-			}
-			return jobsMap, nil
+			return scheduler.ListJobs(), nil
 		}
 
 	manager.commandHandlers[SchedulerRemoveJob] =
