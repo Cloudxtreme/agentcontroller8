0865164e8f83811f6fe834d3b4fc82b6b304a41a
Amr Hassan <amr.hassan@gmail.com>
2015-12-14 14:44:57 +0200

    newclient: More fool-proof measures against resource leakage

--- client/client.go
+++ client/client.go
@@ -50,70 +50,70 @@ func (client Client) LiveAgents() (<-chan []core.AgentID, <-chan error) {
 	return agentsChan, errChan
 }
 
-// Only call the returned value if you're going to exhaust the two returned channels
 func (client Client) ExecuteExecutable(target commandfactory.CommandTarget,
-	executable string, args []string) func() (<-chan responseparsing.ExecutableResult, <-chan error) {
+	executable string, args []string) (<-chan []responseparsing.ExecutableResult, <-chan []error) {
 
 	// Expecting as many responses as there are targeted agents
 
-	command := commandfactory.CommandExecute(target, executable, args)
-	responses := TerminalResponses(client.LowLevelClient.Execute(command))
+  errChan := make(chan []error, 1)
+  responseChan := make(chan []responseparsing.ExecutableResult, 1)	// Must be 1-buffered
 
-	return func() (<-chan responseparsing.ExecutableResult, <-chan error) {
-		errChan := make(chan error)
-		responseChan := make(chan responseparsing.ExecutableResult)
+  go func() {
+    defer close(errChan)
+    defer close(responseChan)
 
-		go func() {
-			defer close(errChan)
-			defer close(responseChan)
+    command := commandfactory.CommandExecute(target, executable, args)
+    responses := exhaust(TerminalResponses(client.LowLevelClient.Execute(command)))
 
-			for {
-				select {
-				case response, isOpen := <-responses:
-					if !isOpen {
-						return
-					}
-					if response.Content.State == core.CommandStateError {
-						errChan <- fmt.Errorf(response.Content.Data)
-					} else {
-						responseChan <- responseparsing.Execute(&response)
-					}
-				}
-			}
-		}()
+    results := []responseparsing.ExecutableResult{}
+    errors := []error{}
 
-		return responseChan, errChan
-	}
+    for _, response := range responses {
+      if response.Content.State == core.CommandStateError {
+        errors = append(errors, fmt.Errorf(response.Content.Data))
+      } else {
+        results = append(results, responseparsing.Execute(&response))
+      }
+    }
+
+    responseChan <- results
+    if len(errors) > 0 {
+      errChan <- errors
+    }
+  }()
+
+  return responseChan, errChan
 }
 
-// You must make sure you exhaust the returned two channels or else resources will leak
-func (client Client) GetProcessStats(target commandfactory.CommandTarget) (<-chan []responseparsing.RunningCommandStats, <-chan error) {
+func (client Client) GetProcessStats(target commandfactory.CommandTarget) (<-chan [][]responseparsing.RunningCommandStats, <-chan []error) {
 
 	// Expecting as many responses as there are targeted agents
 
-	errChan := make(chan error)
-	responseChan := make(chan []responseparsing.RunningCommandStats)
-
-	command := commandfactory.CommandGetProcessStats(target)
-	responses := TerminalResponses(client.LowLevelClient.Execute(command))
+	errChan := make(chan []error)
+	responseChan := make(chan [][]responseparsing.RunningCommandStats)
 
 	go func() {
 		defer close(errChan)
 		defer close(responseChan)
 
-		for {
-			select {
-			case response, isOpen := <-responses:
-				if !isOpen {
-					return
-				}
-				if response.Content.State == core.CommandStateError {
-					errChan <- fmt.Errorf(response.Content.Data)
-				} else {
-					responseChan <- responseparsing.GetProcessStats(&response)
-				}
-			}
-		}
+    command := commandfactory.CommandGetProcessStats(target)
+    responses := exhaust(TerminalResponses(client.LowLevelClient.Execute(command)))
+
+    results := [][]responseparsing.RunningCommandStats{}
+    errors := []error{}
+
+    for _, response := range responses {
+     if response.Content.State == core.CommandStateError {
+       errors = append(errors, fmt.Errorf(response.Content.Data))
+      } else {
+       results = append(results, responseparsing.GetProcessStats(&response))
+      }
+    }
+
+    responseChan <- results
+    if len(errors) > 0 {
+      errChan <- errors
+    }
 	}()
 
 	return responseChan, errChan
@@ -126,26 +126,24 @@ func (client Client) SchedulerListJobs() (<-chan []scheduling.Job, <-chan error)
 	errChan := make(chan error, 1)
 	responseChan := make(chan []scheduling.Job, 1)
 
-	command := commandfactory.CommandInternalSchedulerListJobs()
-	responses := TerminalResponses(client.LowLevelClient.Execute(command))
-
 	go func() {
 		defer close(errChan)
 		defer close(responseChan)
 
-		for {
-			select {
-			case response, isOpen := <-responses:
-				if !isOpen {
-					return
-				}
-				if response.Content.State == core.CommandStateError {
-					errChan <- fmt.Errorf(response.Content.Data)
-				} else {
-					responseChan <- responseparsing.InternalSchedulerListJobs(&response)
-				}
-			}
-		}
+   	command := commandfactory.CommandInternalSchedulerListJobs()
+    responses := exhaust(TerminalResponses(client.LowLevelClient.Execute(command)))
+
+    if len(responses) == 0 {
+      errChan <- fmt.Errorf("Did not receive a response")
+      return
+    }
+
+    response := responses[0]
+    if response.Content.State == core.CommandStateError {
+      errChan <- fmt.Errorf(response.Content.Data)
+    } else {
+      responseChan <- responseparsing.InternalSchedulerListJobs(&response)
+    }
 	}()
 
 	return responseChan, errChan
@@ -180,21 +178,21 @@ func (client Client) SchedulerAddJob(id string, scheduledCommand *core.Command, 
 
 	errChan := make(chan error, 1)
 
-	command := commandfactory.CommandInternalSchedulerAdd(id, scheduledCommand, timingSpec)
-	responses := TerminalResponses(client.LowLevelClient.Execute(command))
-
 	go func() {
 		defer close(errChan)
 
-		select {
-		case response, isOpen := <-responses:
-			if !isOpen {
-				return
-			}
-			if response.Content.State == core.CommandStateError {
-				errChan <- fmt.Errorf(response.Content.Data)
-			}
-		}
+   	command := commandfactory.CommandInternalSchedulerAdd(id, scheduledCommand, timingSpec)
+    responses := exhaust(TerminalResponses(client.LowLevelClient.Execute(command)))
+
+    if len(responses) == 0 {
+      return
+    }
+
+    response := responses[0]
+
+    if response.Content.State == core.CommandStateError {
+      errChan <- fmt.Errorf(responses[0].Content.Data)
+    }
 	}()
 
 	return errChan
@@ -207,23 +205,18 @@ func (client Client) SchedulerRemoveJob(id string) (chan bool, <-chan error) {
 	errChan := make(chan error, 1)
 	responseChan := make(chan bool, 1)
 
-	// This client is receiving older responses
-
-	command := commandfactory.CommandInternalSchedulerRemoveJob(id)
-	responses := TerminalResponses(client.LowLevelClient.Execute(command))
-
 	go func() {
 		defer close(errChan)
 		defer close(responseChan)
 
-		select {
-		case response := <-responses:
-			if response.Content.State == core.CommandStateError {
-				errChan <- fmt.Errorf(response.Content.Data)
-			} else {
-				responseChan <- responseparsing.InternalSchedulerRemoveJob(&response)
-			}
-		}
+    command := commandfactory.CommandInternalSchedulerRemoveJob(id)
+    response := exhaust(TerminalResponses(client.LowLevelClient.Execute(command)))[0]
+
+    if response.Content.State == core.CommandStateError {
+      errChan <- fmt.Errorf(response.Content.Data)
+    } else {
+      responseChan <- responseparsing.InternalSchedulerRemoveJob(&response)
+    }
 	}()
 
 	return responseChan, errChan
